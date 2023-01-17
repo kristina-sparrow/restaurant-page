2d46f3cb026720cbdbdb0c6a3675a664e1de275e
kristina-sparrow <118679841+kristina-sparrow@users.noreply.github.com>
2023-01-16 21:28:39 -0700

    Render nav and footer

--- src/footer.js
+++ src/footer.js
@@ -1,7 +1,12 @@
 import { $content, createHtmlElement } from "./index";
 
 function renderFooter() {
-  //do something
+  const $footer = document.createElement("footer");
+  const $p1 = createHtmlElement("p", null, null, "123 Fake St");
+  const $p2 = createHtmlElement("p", null, null, "Denver, CO");
+  $footer.appendChild($p1);
+  $footer.appendChild($p2);
+  $content.appendChild($footer);
 }
 
 export { renderFooter };
--- src/nav.js
+++ src/nav.js
@@ -1,7 +1,14 @@
 import { $content, createHtmlElement } from "./index";
 
 function renderNav() {
-  //do something
+  const navItems = ["Home", "Menu", "Contact"];
+  const $ul = document.createElement("ul");
+  navItems.forEach((item) =>
+    $ul.appendChild(createHtmlElement("li", null, null, item))
+  );
+  const $nav = document.createElement("nav");
+  $nav.appendChild($ul);
+  $content.appendChild($nav);
 }
 
 export { renderNav };
